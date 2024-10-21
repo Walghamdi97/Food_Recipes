110fe615d223fcef78e20fda5116e0473445d942
Walghamdi97 <wejgh1997@gmail.com>
2024-10-21 12:39:58 +0300

    Initial Commit

---
+++ Food_Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food_Recipes
+//
+//  Created by Wejdan Alghamdi on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food_Recipes/Food_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_RecipesApp.swift
+//  Food_Recipes
+//
+//  Created by Wejdan Alghamdi on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Food_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
