ee4000d9b221eb1633bb6632602d2f718d688140
Walghamdi97 <wejgh1997@gmail.com>
2024-11-04 09:25:16 +0300

    Modification#2

---
+++ Food_Recipes/EditRecipe.swift
@@ -0,0 +1,18 @@
+//
+//  EditRecipe.swift
+//  Food_Recipes
+//
+//  Created by Wejdan Alghamdi on 27/04/1446 AH.
+//
+
+import SwiftUI
+
+struct EditRecipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditRecipe()
+}
---
+++ Food_Recipes/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Food_Recipes
+//
+//  Created by Wejdan Alghamdi on 27/04/1446 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
